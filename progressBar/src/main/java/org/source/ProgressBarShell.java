f8e7a427fc466f02043bcabf9b7bdaeb0a6b4393
delitulmin <delitulmin@gmail.com>
2012-12-11 11:41:53 +0100

    Añadido el proyecto de barra de progreso mavenizado

---
+++ progressBar/src/main/java/org/source/ProgressBarShell.java
@@ -0,0 +1,272 @@
+package org.source;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+/**
+ * Clase para la creación de la barra de progreso en Shell.
+ * 
+ *   <ul>Utilización de la clase :
+ *    <li>(A)Obtenemos la barra:
+ *              <ul>
+ *                 <li>ProgressBarShell.createProgressBar(message);</li>
+ *              </ul>
+ *    </li>
+ *    <li>
+ *        (B)Realizamos la tarea a ejecutar
+ *           <ul>
+ *             <li>do execute jobs ... </li>
+ *           </ul>
+ *    </li>
+ *    <li>(C)Finalizamos la barra de progreso:
+ *       <ul>
+ *         <li>Para ello: ProgressBarShell.finish(messageFinish);</li>
+ *         <li>Si queremos encadenar barras de progreso el message que recibe el createProgressBar(message)
+ *             deberemos obtenerlo una vez ejecutado ProgressBarShell.finish(messageFinish) + "texto a incluir en la nueva barra".
+ *         </li>
+ *       </ul>
+ *    </li>
+ *   </ul>
+ * 
+ * @author victor.duarte 
+ * @email  delitulmin@gmail.com
+ */
+public class ProgressBarShell {
+	
+	/* ==========================================================
+	 * Parámetros estáticos para ProgressBarShell
+	 * ==========================================================*/
+	
+	// Salida estandar.
+	private static PrintStream print = System.out;
+	
+	// Tamaño de la barra de progreso.
+	private static final int MAX_POINTER = 60;
+	
+	// Caracteres para la barra de progreso.
+	private static final char CHARACTER_END_BAR        =']';
+	private static final char CHARACTER_BEGIN_BAR      ='[';
+	private static final char CHARACTER_END_PROGRESS   = '|';
+	private static final char CHARACTER_BEGIN_PROGRESS = '-';
+	
+	// Mensajes a mostrar.
+	private static String MESSAGE_MAIN   = "It's executing:\n\n";
+	private static String MESSAGE_FINISH = "Terminated:\n\n";
+	private static String MESSAGE_RETURN = null;
+		
+	private InnerProgressBar createInnerProgressBar () 
+	{return new InnerProgressBar();}
+	
+	private InnerProgressBar createInnerProgressBar ( String message ) 
+	{return new InnerProgressBar( message );}
+	
+	private static InnerProgressBar monitorPbs = null;
+	public synchronized static void createProgressBar ( String message ) throws Exception 
+	{	
+		if ( monitorPbs != null ) 
+			 throw new RuntimeException( ProgressBarShell.class.getName() + " is already running ..." );
+
+		MESSAGE_RETURN = null;
+		
+		if ( message == null || message.isEmpty() ) {
+			 monitorPbs = new ProgressBarShell().createInnerProgressBar();
+		} else {
+			 monitorPbs = new ProgressBarShell().createInnerProgressBar( message );
+		}
+		
+		Thread th = new Thread( monitorPbs );
+		th.start();
+	}
+	
+	public synchronized static String finish ( String message ) throws Exception
+	{
+		if ( monitorPbs == null ) 
+			 throw new RuntimeException ( ProgressBarShell.class.getName() + " is not running ..." );
+		
+		monitorPbs.doFinish ( message );
+		monitorPbs.doMessageFinish();
+		monitorPbs = null;
+		
+		return MESSAGE_RETURN;
+	}
+	
+	/* =====================================================================
+	 * Clase interna para la generación de la barra de progreso en el Shell.
+	 * =====================================================================
+	 * */
+	private class InnerProgressBar implements Runnable 
+	{	
+		// Monitor para la sincronización.
+		private Object monitor = new Object();
+			
+		//Contenido en byte del shell limpiado.
+		private ByteArrayOutputStream clearShell = null;
+		
+		private InnerProgressBar() {}
+		private InnerProgressBar( String message ) 
+		{ MESSAGE_MAIN = message + "\n"; }
+		
+		private boolean start = true;
+		private void doFinish ( String messageFinish ) 
+		{
+			synchronized (monitor) {
+				MESSAGE_FINISH = messageFinish;
+				this.start     = false;
+			}
+		}
+
+		private String doMessageFinish () throws Exception 
+		{
+			while ( MESSAGE_RETURN == null ) {
+				synchronized ( monitor ) {
+					monitor.wait( 500 );
+				}
+			}
+			return MESSAGE_RETURN;
+		}
+		
+		@Override
+		public void run() 
+		{
+			int points = 1;
+			
+			//1.Limpiamos la pantalla
+			try {
+				doClearShell();
+				print.write( this.clearShell.toByteArray() );
+				print.print( MESSAGE_MAIN );
+			} catch ( Exception ex ) {
+				closeShell();
+				throw new RuntimeException( ex );
+			}
+			
+			//2.Creamos la barra de progreso
+			char[] charArray = doProgressBar ( CHARACTER_BEGIN_PROGRESS, "  0%" );
+			
+			//3.Avanzamos la barra de progreso
+			while ( start ) {
+				try { synchronized ( monitor ) {
+						if ( points > MAX_POINTER ) {
+							//3A.Si llegamos al maximo comenzamos de nuevo
+							doClearShell();
+							print.write( this.clearShell.toByteArray() );
+							print.print( MESSAGE_MAIN );
+							charArray = doProgressBar( CHARACTER_BEGIN_PROGRESS, "  0%" );
+							print.print( new String( charArray ) );
+							points = 1;
+						} else {
+							//3B.Realizamos el progreso de la barra
+							doClearShell();
+							print.write( this.clearShell.toByteArray() );
+							print.print( MESSAGE_MAIN );
+							charArray[ points ] = CHARACTER_END_PROGRESS;
+							
+							//Calculamos el porciento
+							double percent  = percent( points, 0 );
+							
+							//Actualizamos el porciento en el texto
+							String textFine = new String( charArray );
+							
+							int lastClasp   = textFine.lastIndexOf( CHARACTER_END_BAR );
+							textFine        = textFine.substring( 0, lastClasp+1 );
+							textFine       += " " + percent + "%";
+							
+							print.println( textFine );
+							points++;
+						}
+						monitor.wait( 100 );
+					}
+				} catch ( Exception ex ) {
+					closeShell();
+					throw new RuntimeException( ex );
+				}
+			}
+			
+			//4.Antes de salir volvemos a la barra original
+			try {
+				doClearShell();
+				print.write( this.clearShell.toByteArray() );
+				print.print( MESSAGE_MAIN );
+				charArray = doProgressBar( CHARACTER_END_PROGRESS, "100%" );
+				
+				String textProgressBar = new String( charArray );
+				print.println( textProgressBar );
+				print.println( MESSAGE_FINISH );
+				MESSAGE_RETURN = MESSAGE_MAIN + textProgressBar + "\n" + MESSAGE_FINISH; 
+			} catch ( Exception ex ) {
+				throw new RuntimeException( ex );
+			} finally {
+				closeShell();
+			}
+			print.print("\n");
+		}
+		
+		private void closeShell() 
+		{
+			try {
+				this.clearShell.close();
+				this.clearShell = null;
+			} catch ( IOException e ) {
+				this.clearShell = null;
+			} 
+		}
+		
+		private double percent( double number, int decimal ) 
+		{
+			double percent = ((float)number/(float)MAX_POINTER)*100;
+			long mult      = (long)Math.pow(10,decimal);
+			return (Math.round(percent*mult))/(double)mult;
+		} 
+		
+		/*
+		 * Creamos la barra de progreso
+		 */
+		private char[] doProgressBar( char caracter, String fin ) 
+		{
+			StringBuffer sb = new StringBuffer();
+			sb.append( CHARACTER_BEGIN_BAR );
+			for ( int i = 0; i < MAX_POINTER; i++ ) {
+				sb.append( caracter );
+			}
+			sb.append( CHARACTER_END_BAR );
+			sb.append( " " + fin );
+			return sb.toString().toCharArray();
+		}
+		
+		/*
+		 * Nos permite limpiar la ventana del shell.
+		 */
+		private void doClearShell() throws IOException 
+		{
+			if ( this.clearShell != null ) return;
+			
+			InputStream in = null;
+			try {
+				String so      = System.getProperty("os.name");
+				String command = null;
+				
+				if ( so.equals("Windows") ) {
+					 command = "cls";
+				} else {
+					 command = "clear";
+				}
+				
+				Process exec    = Runtime.getRuntime().exec( command );
+				in              = exec.getInputStream();
+				this.clearShell = new ByteArrayOutputStream();
+				
+				int c;
+				byte[] buf = new byte [1024];
+				while ( (c = in.read(buf)) != -1 ) {
+					this.clearShell.write(buf,0,c);
+				}
+			} catch ( Exception ex ) {
+				throw new IOException( ex.getMessage() );
+		    } finally {
+				if (in != null) in.close();
+			}
+		}
+	}
+}
