891835c61cb5abf7deea370c6dfb7b39712feb81
delitulmin <delitulmin@gmail.com>
2012-12-11 14:37:22 +0100

    Optimizamos la clase del test

--- progressBar/src/main/java/org/source/TestProgressBarShell.java
+++ progressBar/src/main/java/org/source/TestProgressBarShell.java
@@ -9,23 +9,16 @@ public class TestProgressBarShell {
 	public static void main ( String[] args )throws Exception 
 	{
 		PrintWriter print = new PrintWriter(System.out);
-		BufferedReader in  = 
+		BufferedReader in = 
 				new BufferedReader(new InputStreamReader(System.in));
 		
-		String message = "Execute test";
+		ProgressBarShell.createProgressBar("Execute test");
 		
-		ProgressBarShell.createProgressBar(message);
+		print.printf("> ");
+		//waiting to enter...
+		in.readLine().trim();
 		
-		boolean condition = true;
-		while ( condition )
-		{	
-			print.printf("> ");
-			
-			//waiting to enter...
-			in.readLine().trim();
-			
-			ProgressBarShell.finish("Finished test");
-			condition = false;
-		}
+	    ProgressBarShell.finish("Finished test");
 	}
 }
+
